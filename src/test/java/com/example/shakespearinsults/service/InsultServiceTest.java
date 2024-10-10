c4f9752bad27a1fa04746dc1853981eff212260d
PNorrmyr <philip.norrmyr@gmail.com>
2024-10-10 10:28:12 +0200

    Added InsultServiceTest.java and main.yml to test working with GitHub Workflows

--- src/test/java/com/example/shakespearinsults/service/InsultServiceTest.java
+++ src/test/java/com/example/shakespearinsults/service/InsultServiceTest.java
@@ -1,12 +1,34 @@
 package com.example.shakespearinsults.service;
 
+import com.example.shakespearinsults.model.Insult;
+import com.example.shakespearinsults.repository.InsultRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class InsultServiceTest {
 
     @Test
     void getAll() {
+        //Given
+        InsultRepository insultRepository = mock(InsultRepository.class);
+        InsultService insultService = new InsultService(insultRepository);
+        List<Insult> expectedList = Arrays.asList(new Insult("Testing"), new Insult("List"), new Insult("Of"), new Insult("Strings"));
+        when(insultRepository.getAll()).thenReturn(expectedList);
+
+
+        //When
+        List<Insult> retrievedList = insultService.getAll();
+
+        //Then
+        assertEquals(expectedList, retrievedList);
+        verify(insultRepository).getAll();
+
     }
 }
